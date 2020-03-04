276e0bf730fe3e03349ffe3ffe29bc9349ecf92d
Daniel Zatovic <daniel.zatovic@gmail.com>
2020-03-04 10:19:24 +0100

    add quicktest

--- tpm2-algtest-ui.py
+++ tpm2-algtest-ui.py
@@ -17,12 +17,11 @@ if __name__ == "__main__":
     group = YUI.widgetFactory().createRadioButtonGroup(vbox)
     type_box = YUI.widgetFactory().createHBox(group)
 
-    # quicktest_button = YUI.widgetFactory().createRadioButton(type_box, "&quicktest")
-    # quicktest_button.setValue(True)
-    # group.addRadioButton(quicktest_button)
+    quicktest_button = YUI.widgetFactory().createRadioButton(type_box, "&quicktest")
+    quicktest_button.setValue(True)
+    group.addRadioButton(quicktest_button)
 
     keygen_button = YUI.widgetFactory().createRadioButton(type_box, "&keygen")
-    keygen_button.setValue(True)
     group.addRadioButton(keygen_button)
 
     perf_button = YUI.widgetFactory().createRadioButton(type_box, "&perf")
@@ -59,12 +58,32 @@ if __name__ == "__main__":
                     dialog.destroy()
                     break
             elif ev.widget() == run_button:
+                if quicktest_button.value():
+                    detail_dir = "out"
+                    os.makedirs(detail_dir, exist_ok=True)
+
+                    run_command = ['tpm2_getcap']
+
+                    getcap_proc = subprocess.Popen(["tpm2_getcap", "-v"], stdout=subprocess.PIPE)
+                    line = getcap_proc.stdout.readline().decode("ascii")
+                    version_begin = line.find('version="') + len('version="')
+                    version_end = line.find('"', version_begin)
+                    version = line[version_begin:version_end].split(".")
+                    version = list(map(int, version))
+
+                    # newer versions take category directly as an argument, older need -c
+                    if version < [4, 0, 0]:
+                        run_command.append("-c")
+
+                    categories = ['algorithms', 'commands', 'properties-fixed', 'properties-variable', 'ecc-curves', 'handles-persistent']
+                    for category in categories:
+                        with open(os.path.join(detail_dir, f'Quicktest_{category}.txt'), 'w') as outfile:
+                            subprocess.run(run_command + [category], stdout=outfile).check_returncode()
+                    continue
                 if algtest_proc is not None and algtest_proc.poll() is None:
                     algtest_proc.terminate()
                 progress_bar.setValue(0)
-                test = "keygen"
-                if perf_button.value():
-                    test = "perf"
+                test = "perf" if perf_button.value() else "keygen"
                 algtest_proc = subprocess.Popen(cmd + [test], stdout=subprocess.PIPE)
                 fd = algtest_proc.stdout.fileno()
                 fl = fcntl.fcntl(fd, fcntl.F_GETFL)
